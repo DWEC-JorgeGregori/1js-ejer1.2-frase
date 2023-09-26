dbc285a0caa34e2d638da1ab1f536b0d22c7813a
jorge <jjlosantos7@gmail.com>
2023-09-26 11:36:34 +0200

    [MOD] index.js

--- scripts/index.js
+++ scripts/index.js
@@ -2,12 +2,15 @@
 
 
 let frase = prompt("introduce una frase");
-
-console.log("Frase: " + "\"" + frase + "\"");
-console.log(letters(frase) + " letras y " + words(frase) + " palabras");
-console.log(upperString(frase));
-console.log(titleString(frase));
-console.log(backwardsLetters(frase));
-console.log(backwardsWords(frase));
-let isPalindrome = palindrome(frase)?"Si":"No";
-console.log(isPalindrome + " es palíndromo");
+if (frase.trim() === "") {
+    console.log("Has de introducir una cadena válida");
+} else {
+    console.log("Frase: " + "\"" + frase + "\"");
+    console.log(letters(frase) + " letras y " + words(frase) + " palabras");
+    console.log(upperString(frase));
+    console.log(titleString(frase));
+    console.log(backwardsLetters(frase));
+    console.log(backwardsWords(frase));
+    let isPalindrome = palindrome(frase)?"Si":"No";
+    console.log(isPalindrome + " es palíndromo");
+}
